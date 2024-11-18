b0a89c3be8c8fc26bbb16423dc5c42704660f4bb
Chandan Kumar <chandantech56@gmail.com>
2024-11-18 11:47:01 +0530

    push

--- python_class5/class5.py
+++ python_class5/class5.py
@@ -24,5 +24,27 @@ print(L[2])
 print(L[2][0])
 print(L[2][2])
 
-#negative index data types
+#negative index data 
+# Negative indexing means start from the end
+# -1 refers to the last item, -2 refers to the second last item etc.
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#Negative indexing means starting from the end of the list.
+#This example returns the items from index -4 (included) to index -1 (excluded)
+#Remember that the last item has the index -1,
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#This will return the items from index 0 to index 4.
+#Remember that index 0 is the first item, and index 4 is the fifth item
+#Remember that the item in index 4 is NOT included
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+
+#This will return the items from index 2 to the end.
+#Remember that index 0 is the first item, and index 2 is the third
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
 
