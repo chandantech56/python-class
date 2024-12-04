c37ce414915719b952ce4c7d383892bcb1d5d7d5
chandan <chandantech56@gmail.com>
2024-12-04 11:03:30 +0530

    push

--- python_class13/class13.py
+++ python_class13/class13.py
@@ -1,4 +1,13 @@
 #File : file is collection of data. 
+
+# mode: Specifies the purpose for opening the file:
+# 'r': Read (default mode).
+# 'w': Write (overwrites if the file exists).
+# 'a': Append.
+# 'b': Binary mode (use with 'r', 'w', or 'a').
+# 'x': Create a new file (fails if the file exists).
+# 't': Text mode (default, used with 'r', 'w', or 'a').
+
 with open("hello_world.txt", "w") as f:
     f.write("Hello world!, How are you. ")
     print(f)
