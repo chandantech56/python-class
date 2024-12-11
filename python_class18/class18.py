c4770d79351d0b5c17df17bc31419ca0838b8ec1
chandan <chandantech56@gmail.com>
2024-12-11 17:31:00 +0530

    commit

--- python_class18/class18.py
+++ python_class18/class18.py
@@ -1,13 +1,3 @@
-fruits_list= ["mango","banana","grapes"]
-for char in fruits_list[1]:
-     print(char)
-
-class car:
-     def car(self):
-          self.car= car
-print(f"{car} has start")
-
-
 #api
 import requests
 response = requests.get("https://www.randomuser.me/api/")
