dde84b7580b8497d99b3277a2f390bd0cd4b1103
chandan <chandantech56@gmail.com>
2024-12-11 08:02:00 +0530

    push

---
+++ python_class18/class18.py
@@ -0,0 +1,17 @@
+fruits_list= ["mango","banana","grapes"]
+for char in fruits_list[1]:
+     print(char)
+
+class car:
+     def car(self):
+          self.car= car
+print(f"{car} has start")
+
+
+#api
+import requests
+response = requests.get("https://www.randomuser.me/api/")
+print(response)
+response.text
+
+#for install :   pip install requests
